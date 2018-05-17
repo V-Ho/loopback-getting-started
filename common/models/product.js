a740af5486a7aa561c5f518dce060349f2de6a4c
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-05-17 14:28:03 +0200

    add comments to buy remote method

--- common/models/product.js
+++ common/models/product.js
@@ -1,53 +1,29 @@
 'use strict';
 
-// module.exports = function(Product) {
-//
-// /**
-// * Return true if the input is larger than zero
-// * @param {number} quantity Number to validate
-// */
-//
-// //helper method to validate quantity
-// const validQuantity = quantity => Boolean(quantity > 0)
-//
-//  /**
-// * Buy this product
-// * @param {number} quantity Number of products to buy
-// * @param {Function(Error, object)} callback
-// */
-//
-//   Product.prototype.buy = function(quantity, callback) {
-//     //invoke helper method in
-//     if (!validQuantity(quantity)) {
-//       return callback(`Invalid quantity ${quantity}`)
-//     }
-//     const result = {
-//       status: `You bought ${quantity} product(s)`
-//     }
-//     callback(null, result);
-//   };
-// }
-
-
 module.exports = function(Product) {
-  /**
-  * Return true if input is larger than zero
-  * @param {number} quantity Number to validate
-  */
-  const validQuantity = quantity => Boolean(quantity > 0);
 
-  /**
-   * Buy this product
-   * @param {number} quantity Number of products to buy
-   * @param {Function(Error, object)} callback
-   */
+/**
+* Return true if the input is larger than zero
+* @param {number} quantity Number to validate
+*/
+
+//helper method to validate quantity
+const validQuantity = quantity => Boolean(quantity > 0)
+
+ /**
+* Buy this product
+* @param {number} quantity Number of products to buy
+* @param {Function(Error, object)} callback
+*/
+
   Product.prototype.buy = function(quantity, callback) {
+    //invoke helper method in
     if (!validQuantity(quantity)) {
       return callback(`Invalid quantity ${quantity}`)
     }
     const result = {
-      status: `You bought ${quantity} product(s)`,
-    };
+      status: `You bought ${quantity} product(s)`
+    }
     callback(null, result);
   };
 }
